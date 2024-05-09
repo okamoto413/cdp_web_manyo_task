12cdd5595e119b017e91e6cec3c2d018a1e37bd0
kujira12 <ha9botan@gmail.com>
2024-05-09 18:04:27 +0900

    FactoryBotによるデータ作成

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '登録機能' do
     context 'タスクを登録した場合' do
       it '登録したタスクが表示される' do
-      task = Task.new(title:'タイトル', content:'内容')
+      #task = Task.new(title:'タイトル', content:'内容')
+      # FactoryBotを使用してタスクのインスタンスを生成
+      task = FactoryBot.create(:task, title: 'タイトル', content:'内容')
       expect(task.title).to eq 'タイトル'
       expect(task.content) .to eq '内容'
       end
@@ -15,7 +17,8 @@ RSpec.describe 'タスク管理機能', type: :system do
     context '一覧画面に遷移した場合' do
       it '登録済みのタスク一覧が表示される' do
         # テストで使用するためのタスクを登録
-        Task.create!(title: '書類作成', content: '企画書を作成する。')
+        #Task.create!(title: '書類作成', content: '企画書を作成する。')
+        FactoryBot.create(:task)
         # タスク一覧画面に遷移
         visit tasks_path
         # visit（遷移）したpage（この場合、タスク一覧画面）に"書類作成"という文字列が、have_content（含まれていること）をexpect（確認・期待）する
@@ -28,7 +31,9 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it 'そのタスクの内容が表示される' do
-       task = Task.create!(title: '詳細タスク', content: '詳細内容')
+       #task = Task.create!(title: '詳細タスク', content: '詳細内容')
+      # FactoryBotを使用してタスクのインスタンスを生成
+       task = FactoryBot.create(:task, title: '詳細タスク', content:'詳細内容')
        visit task_path(task)
        expect(page).to have_content '詳細タスク'
        expect(page).to have_content '詳細内容'
