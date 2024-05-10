913e5f913903e8ba3bf211803038178e92731626
kujira12 <ha9botan@gmail.com>
2024-05-10 18:18:55 +0900

    Model spec 修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -12,13 +12,15 @@ RSpec.describe 'タスクモデル機能', type: :model do
 
     context 'タスクの説明が空文字の場合' do
       it 'バリデーションに失敗する' do
-        task = Task.create(title: '', content: '企画書を作成する。')
+        task = Task.create(title: 'タスクタイトル', content: '')
         expect(task).not_to be_valid
       end
     end
 
     context 'タスクのタイトルと説明に値が入っている場合' do
       it 'タスクを登録できる' do
+        task = Task.create(title: 'タスクタイトル', content: 'タスク説明')
+        expect(task).to be_valid        
       end
     end
   end
