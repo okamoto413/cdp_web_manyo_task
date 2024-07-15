ec7ab4be08022d79ab9a4ae38af4e7d5ccbc72c0
kujira12 <ha9botan@gmail.com>
2024-07-16 00:17:20 +0900

    ラベルチェックボックス修正

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,7 +1,7 @@
 # app/models/label.rb
 class Label < ApplicationRecord
   belongs_to :user
-  has_many :task_labels
+  has_many :task_labels, dependent: :destroy
   has_many :tasks, through: :task_labels, source: :task
 
   validates :name, presence: true
