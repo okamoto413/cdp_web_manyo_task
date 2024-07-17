95b89779be7596318dc672f27113ad67d4e68160
kujira12 <ha9botan@gmail.com>
2024-07-17 18:14:51 +0900

    System spec　label 修正

--- spec/system/labels_spec.rb
+++ spec/system/labels_spec.rb
@@ -1,7 +1,16 @@
  require 'rails_helper'
  RSpec.describe 'ラベル管理機能', type: :system do
    
-   
+    let!(:user) { FactoryBot.create(:user) }  
+    let!(:label) { FactoryBot.create(:label, name: 'テストラベル', user: user ) }  
+ 
+    before do
+    visit new_session_path
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: user.password
+    click_button 'ログイン'
+    end 
+
     describe '登録機能' do
      context 'ラベルを登録した場合' do
        it '登録したラベルが表示される' do
@@ -16,8 +25,8 @@
    describe '一覧表示機能' do
      context '一覧画面に遷移した場合' do
        it '登録済みのラベル一覧が表示される' do
-          FactoryBot.create(:label, name: 'ラベル1')
-          FactoryBot.create(:label, name: 'ラベル2')
+          FactoryBot.create(:label, name: 'ラベル1', user: user )
+          FactoryBot.create(:label, name: 'ラベル2', user: user )
           visit labels_path
           expect(page).to have_content 'ラベル1'
           expect(page).to have_content 'ラベル2'
